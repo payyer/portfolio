d7a601fcc03072aea3cbf8941e21fcc572573ced
payyer <quocanhle112.com>
2026-04-12 10:24:32 +0700

    chore: update title job

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,7 +6,7 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
+          "Front-end Developer",
           "Freelancer",
         ],
         autoStart: true,
